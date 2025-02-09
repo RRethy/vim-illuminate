4a3b3983515676cb1f2730b31dc5e7071321465c
linrongbin16 <linrongbin16@outlook.com>
2025-02-09 02:10:26 +0000

    fix(treesitter): use 'pcall' to tolerate require nvim-treesitter exception (#223)

--- lua/illuminate/providers/treesitter.lua
+++ lua/illuminate/providers/treesitter.lua
@@ -1,5 +1,3 @@
-local locals = require('nvim-treesitter.locals')
-
 local M = {}
 
 local buf_attached = {}
@@ -7,6 +5,11 @@ local buf_attached = {}
 -- get_node is builtin in v0.9+, get_node_at_cursor is for older versions
 local get_node_at_cursor = vim.treesitter.get_node or require('nvim-treesitter.ts_utils').get_node_at_cursor
 function M.get_references(bufnr)
+    local ok, locals = pcall(require, 'nvim-treesitter.locals')
+    if not ok then
+        return
+    end
+
     local node_at_point = get_node_at_cursor()
     if not node_at_point then
         return
